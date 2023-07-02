df580fc0db83dc7e70effe1b4fcf32a67bd0cda4
wenxig <117057186+wenxig@users.noreply.github.com>
2023-07-02 21:07:03 +0800

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.ts";
+import { createPinia } from "pinia";
+const pinia = createPinia();
+const app = createApp(App);
+app.use(router);
+pinia.use(({ store }) => {
+  const initialState = JSON.parse(JSON.stringify(store.$state));
+  store.$reset = () => {
+    store.$state = JSON.parse(JSON.stringify(initialState));
+  }
+});
+app.use(pinia);
+app.mount("#app");
---
+++ src/pages/home/train/listItem/listbox.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+    <ul>
+      <li>
+        <p>名称</p>
+        <p>满分</p>
+        <p>次数</p>
+        <p>平均成绩</p>
+        <p>最高成绩</p>
+        <p>是否合格</p>
+        <p>开始练习</p>
+      </li>
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main {
+  background-color: transparent !important;
+  & ul {
+    margin: 0%;
+    padding: 0px;
+    width: 100%;
+  }
+  & li {
+    margin: 0 auto;
+    display: flex;
+    width: 95%;
+    height: 40px;
+    background-color: rgba($color: #979797, $alpha: 0.2);
+    margin-top: 10px;
+    justify-content: space-around;
+    align-items: center;
+    & > p {
+      font-size: 17px;
+      width: 11%;
+    }
+    & > p:first-child {
+      width: 20%;
+    }
+  }
+}
+:deep(li) {
+  width: 95%;
+  height: 40px;
+  margin: 0 auto;
+}
+</style>
