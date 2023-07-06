e3284be55376fda28b50ace765b6b6e5fc488901
wenxig <117057186+wenxig@users.noreply.github.com>
2023-07-06 17:20:26 +0800

    Add files via upload

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+use tauri::Manager;
+// Create the command:
+// This command must be async so that it doesn't run on the main thread.
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // Show main window
+  window.get_window("main").unwrap().show().unwrap();
+}
+
+// Register the command:
+fn main() {
+  tauri::Builder::default()
+    // Add this line
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
+}
